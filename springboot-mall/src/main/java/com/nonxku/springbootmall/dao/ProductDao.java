13a2d8c85d14b9da4ff7672664c13167e1548e9f
nonxku <nonxku@gmail.com>
2025-10-27 16:18:56 +0800

    商品功能-實作查詢商品列表（基礎實作）

--- springboot-mall/src/main/java/com/nonxku/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/nonxku/springbootmall/dao/ProductDao.java
@@ -3,11 +3,15 @@ package com.nonxku.springbootmall.dao;
 import com.nonxku.springbootmall.dto.ProductRequest;
 import com.nonxku.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
+
 
 }
--- springboot-mall/src/main/java/com/nonxku/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/nonxku/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.nonxku.springbootmall.service;
 import com.nonxku.springbootmall.dto.ProductRequest;
 import com.nonxku.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
