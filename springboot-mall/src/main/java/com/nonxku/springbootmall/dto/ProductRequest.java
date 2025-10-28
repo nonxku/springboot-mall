ae8e1e6b12e338bb3bdef2e7e8a0985c17c48c64
nonxku <nonxku@gmail.com>
2025-10-28 22:35:13 +0800

    商品功能-單元測試

--- springboot-mall/src/main/java/com/nonxku/springbootmall/dto/ProductRequest.java
+++ springboot-mall/src/main/java/com/nonxku/springbootmall/dto/ProductRequest.java
@@ -1,7 +1,7 @@
 package com.nonxku.springbootmall.dto;
 
 import com.nonxku.springbootmall.constant.ProductCategory;
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 
 public class ProductRequest {
